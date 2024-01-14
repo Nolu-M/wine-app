4b082206c137e0958d2f0b252681968ad721c119
Nolubabalo Maxazana <nolubabalomaxazana@gmail..com>
2024-01-14 18:49:56 +0200

    Fixed New Wines page.

--- src/Pages/NewWine.jsx
+++ src/Pages/NewWine.jsx
@@ -42,28 +42,34 @@ function NewWine() {
 
 
   return (
-    <section className="container mx-auto">
-      <div className=''>
+    <section className="container mx-auto flex justify-center">
+      <div className='mt-6'>
         <form>
           <div>
             <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
+            <div className='border-gray border-2 rounded-xl'>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className='w-full'
+              />
+              </div>
           </div>
           <div>
             <label htmlFor="year">Year</label>
-            <input
-              type="text"
-              id="year"
-              name="year"
-              value={formData.year}
-              onChange={handleChange}
-            />
+            <div className='border-gray border-2 rounded-xl'>
+              <input
+                type="text"
+                id="year"
+                name="year"
+                value={formData.year}
+                onChange={handleChange}
+                className='w-full'
+              />
+              </div>
           </div>
           <div>
             <label htmlFor="type">Type</label>
@@ -73,6 +79,7 @@ function NewWine() {
               name="type"
               value={formData.name}
               onChange={handleChange}
+              className='w-full border-gray border-2 rounded-xl pt-2 pb-2'
             >
               <option value="">Select Type</option>
               <option value="Red">Red</option>
@@ -82,32 +89,39 @@ function NewWine() {
           </div>
           <div>
             <label htmlFor="varietal">Varietal</label>
+            <div className='border-gray border-2 rounded-xl '>
             <input
               type="text"
               id="varietal"
               name="varietal"
               value={formData.varietal}
-              onChange={handleChange}
-            />
+                onChange={handleChange}
+                className='w-full'
+              />
+              </div>
           </div>
           <div>
             <label htmlFor="rating">Rating</label>
+            <div className='border-gray border-2 rounded-xl'>
             <input
               type="text"
               id="rating"
               name="rating"
               value={formData.rating}
-              onChange={handleChange}
-            />
+                onChange={handleChange}
+                className='w-full'
+              />
+              </div>
           </div>
-          <div>
-            <label htmlFor="consumed">Consumed</label>
+          <div className='pt-4 pb-4'>
+            <label htmlFor="consumed" className='mr-2'>Consumed</label>
             <input
               type="checkbox"
               id="consumed"
               name="consumed"
               value={formData.consumed}
               onChange={handleChange}
+              className=''
             />
           </div>
           <div>
@@ -119,18 +133,18 @@ function NewWine() {
               onChange={handleChange}
             />
           </div>
-          <div className='flex justify-between mt-4'>
+          <div className='flex justify-between mt-6'>
             <button
               type="button"
               onClick={handleCancel}
-              className=''
+              className='p-4 bg-[#DF1C41] rounded-lg w-1/2 mr-4'
             >
               Cancel
             </button>
             <button
               type='button'
               onClick={handleSave}
-              className=''
+              className='p-4 bg-[#DF1C41] rounded-lg w-1/2'
             >
               Save
             </button>
